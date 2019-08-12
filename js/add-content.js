5bd11945d8c504c2e071a8d726f8c5181cf3d94f
dpastuszka <48692793+dpastuszka@users.noreply.github.com>
2019-08-12 22:52:33 +0200

    2

--- js/add-content.js
+++ js/add-content.js
@@ -1,4 +1,4 @@
-(function ziemniaczek() {
+(function () {
 
 var hotel = {
     name : 'Hotel',
@@ -21,4 +21,28 @@ roomPrice.textContent = hotel.roomPrice.toFixed(2) + ' zł';
 
 specialPrice.textContent = hotel.offerPrice() + ' zł';
 
-});
+var expiryMsg;
+var today;
+var elEnds;
+
+function offerExpires(today) {
+    var weekFromToday, day, date, month, year, dayNames, monthNames;
+    weekFromToday = newDate(today.getTime() + 7 * 24 * 60 * 60 * 1000);
+    dayNames = ['niedzielę', 'poniedziałek', 'wtorek', 'środę', 'czwartek', 'piątek', 'sobotę'];
+    monthNames = ['stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'września', 'października', 'listopada', 'grudnia'];
+
+    day = dayNames[weekFromToday.getDay()];
+    date = weekFromToday.getDate();
+    month = monthNames[weekFromToday.getMonth()];
+    year = weekFromToday.getFullYear();
+    expiryMsg = 'Oferta wygasa w: ';
+    expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
+    return expiryMsg;
+}
+
+today = newDate();
+
+elEnds = document.getElementById('offerEnds');
+elEnds.innerHTML = offerExpires(today);
+
+}());
