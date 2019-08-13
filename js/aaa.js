ce35f58c8698beb0651a694793d22ef5983109f1
dpastuszka <48692793+dpastuszka@users.noreply.github.com>
2019-08-13 22:00:19 +0200

    4

---
+++ js/aaa.js
@@ -0,0 +1,14 @@
+alert('hello');
+
+(function () {
+
+  var hotelName, control;
+
+  hotelName = document.getElementById('hotelName');
+  control = document.getElementById('control');
+  
+  hotelName.textContent = "Ziemniaczek";
+  control.textContent = "Kontrola";
+
+
+}());
