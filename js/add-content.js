480ca23edc88b15fce1b926cd876155490454d1e
dpastuszka <48692793+dpastuszka@users.noreply.github.com>
2019-08-16 21:48:09 +0200

    4

--- js/add-content.js
+++ js/add-content.js
@@ -1,48 +1,8 @@
-(function () {
+var pass = 50;
 
-var hotel = {
-    name : 'Hotel',
-    roomPrice: 500,
-    discount: 30,
-    offerPrice: function () {
-        var offerRate = this.roomPrice * ((100 - this.discount) / 100);
-        return offerRate;
-    }
-}
+var score = 20;
 
-var hotelName, roomPrice, specialPrice;
+var hasPassed = score >= pass;
 
-hotelName = document.getElementById('hotelName');
-roomPrice = document.getElementById('roomPrice');
-specialPrice = document.getElementById('specialPrice');
-
-hotelName.textContent = hotel.name;
-roomPrice.textContent = hotel.roomPrice.toFixed(2) + ' zł';
-
-specialPrice.textContent = hotel.offerPrice().toFixed(2) + ' zł';
-
-var expiryMsg;
-var today;
-var elEnds;
-
-function offerExpires(today) {
-    var weekFromToday, day, date, month, year, dayNames, monthNames;
-    weekFromToday = new Date(today.getTime() + 7 * 24 * 60 * 60 * 1000);
-    dayNames = ['niedzielę', 'poniedziałek', 'wtorek', 'środę', 'czwartek', 'piątek', 'sobotę'];
-    monthNames = ['stycznia', 'lutego', 'marca', 'kwietnia', 'maja', 'czerwca', 'lipca', 'sierpnia', 'września', 'października', 'listopada', 'grudnia'];
-
-    day = dayNames[weekFromToday.getDay()];
-    date = weekFromToday.getDate();
-    month = monthNames[weekFromToday.getMonth()];
-    year = weekFromToday.getFullYear();
-    expiryMsg = 'Oferta wygasa w: ';
-    expiryMsg += day + ' <br />(' + date + ' ' + month + ' ' + year + ')';
-    return expiryMsg;
-}
-
-today = new Date();
-
-elEnds = document.getElementById('offerEnds');
-elEnds.innerHTML = offerExpires(today);
-
-}());
+var el = document.getElementById('decision');
+el.textContent = 'Test zaliczony: ' + hasPassed;
